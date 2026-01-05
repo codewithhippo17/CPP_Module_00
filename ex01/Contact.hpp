be295684df9a85fc719a13ccd68a70678d708b2d
ehamza <ehamza@student.1337.ma>
2026-01-05 18:14:02 +0100

    refactor(Contact): remove display methods from class interface

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -1,30 +1,31 @@
 
 #ifndef CONTACT_HPP
-# define CONTACT_HPP
+#define CONTACT_HPP
 
-# include <iostream>
-# include <string>
+#include <iostream>
+#include <string>
 
-class Contact
-{
-  private:
-	std::string firstName;
-	std::string lastName;
-	std::string nickName;
-	std::string phoneNumber;
-	std::string darkestSecret;
+class Contact {
+private:
+  std::string firstName;
+  std::string lastName;
+  std::string nickName;
+  std::string phoneNumber;
+  std::string darkestSecret;
 
-  public:
-	Contact();
+public:
+  Contact();
 
-	void setContact(const std::string fn, const std::string ln,
-		const std::string nn, const std::string pn, const std::string ds);
+  void setContact(const std::string fn, const std::string ln,
+                  const std::string nn, const std::string pn,
+                  const std::string ds);
 
-	std::string getFirstName() const;
-	std::string getLastName() const;
-	std::string getNickName() const;
-
-	void displayFullContact() const;
-	void displayTableEntry(int index) const;
+  std::string getFirstName() const;
+  std::string getLastName() const;
+  std::string getNickName() const;
 };
+
+void displayFullContact();
+void displayTableEntry(int index);
+
 #endif
